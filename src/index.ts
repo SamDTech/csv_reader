96f992638918e67368244bac24c310ae48bc6e06
Damilare <opeyemisamuel222@gmail.com>
2021-03-09 16:42:37 +0000

    analyze and print report

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,15 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (const match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+
+console.log(`Man United won ${manUnitedWins} times`)
