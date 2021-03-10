42e0c829d4fc1530f0b1eee35aa10f33df099fbf
Damilare <opeyemisamuel222@gmail.com>
2021-03-10 18:02:58 +0000

    building the report

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,3 +1,4 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { MatchData } from './MatchData';
 
 export interface Analyzer {
@@ -8,7 +9,12 @@ export interface OutputTarget {
   print(report: string): void;
 }
 
-
 export class Summary {
-  constructor(public analyzer: Analyzer; public outputTarget: OutputTarget){}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+
+    this.outputTarget.print(output)
+  }
 }
